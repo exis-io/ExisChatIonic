3a379d66872c9ae12ff6a0a918d9e247cc64c16d
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-15 10:11:28 -0500

    using want with join events

--- www/js/controller.js
+++ www/js/controller.js
@@ -7,38 +7,46 @@ angular.module('exisChat.controller', [])
   //Login anonymously to Exis (requires Auth appliance level 0)
   $riffle.login().then(join);
 
+  //notify everyone we've joined
+  function join(){
+    $riffle.publish('exisChat', $riffle.user.username());
+    $scope.welcome = "Welcome " + $riffle.user.username() + "!";
+  }
+
   //a handler that only wants message objects
   var wantMsg = $riffle.want(receiveMsg, {username: String, msg: String});
 
   //a handler that only wants join events
   var wantJoinEvent = $riffle.want(joinEvent, String);
 
+  //subscribe for out 2 different types of msgs
+  $riffle.subscribe('exisChat', wantMsg);
+  $riffle.subscribe('exisChat', wantJoinEvent);
+
   //handle messages here
   function receiveMsg(msg){
     msg.received = true;
-    $scope.msgs.push(msg);
-    $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom();
+    displayMsg(msg);
   }
 
   //handle join events
   function joinEvent(user){
     var msg ={username: user, join: true};
-    $scope.msgs.push(msg);
+    displayMsg(msg);
   }
    
-  function join(){
-    $riffle.publish('exisChat', $riffle.user.username());
-    $scope.welcome = "Welcome " + $riffle.user.username() + "!";
-  }
 
-  $riffle.subscribe('exisChat', wantMsg);
-  $riffle.subscribe('exisChat', wantJoinEvent);
-
+  //send messages
   $scope.sendMsg = function(text){
     var msg = {username: $riffle.user.username(), msg: text}
     $riffle.publish('exisChat', msg);
-    $scope.msgs.push(msg);
     $scope.input.msg = "";
-    $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom();
+    displayMsg(msg);
+  }
+
+  //display messages
+  function displayMsg(msg){
+    $scope.msgs.push(msg);
+    $ionicScrollDelegate.scrollBottom();
   }
 });
